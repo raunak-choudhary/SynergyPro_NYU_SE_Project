bac0a2c9a2410d2884b1979e47403f06411fab73
Yogarajalakshmi S <yogarajalakshmis@gmail.com>
2024-10-13 16:47:45 -0400

    view tasks (list and individual) functionality

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('tasks/create/', views.create_task, name='create_task'),
+    path('tasks', views.task_list, name='task_list'),
+    path('tasks/<int:task_id>', views.task_detail, name='task_detail')
 ]
 
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,17 +1,27 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 
 # Create your views here.
+# create task
 def create_task(request):
     if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('/tasks')
     else:
         form = TaskForm()
 
     return render(request, 'task_manager/create_task.html', {'form': form})
 
+# view all tasks
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_manager/task_list.html', {'tasks': tasks})
+
+# view individual task
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    return render(request, 'task_manager/task_detail.html', {'task': task})
 
