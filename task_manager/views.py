0e08b0a6884d0a2a01419fba2a54579d3703663c
Yogarajalakshmi S <yogarajalakshmis@gmail.com>
2024-10-13 13:01:21 -0400

    basic task creation form

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
+def create_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+
+    return render(request, 'task_manager/create_task.html', {'form': form})
+
+
