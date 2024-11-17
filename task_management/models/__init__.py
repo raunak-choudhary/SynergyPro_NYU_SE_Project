e3a567958ef4b430fd408cf2bf5df55403ffbbe2
Raunak Choudhary <63695296+raunak-choudhary@users.noreply.github.com>
2024-11-17 12:38:52 -0500

    Push 2
    Includes updates on Notification Bar in Dashboard and Profile Page; 
    User can upload Profile Photo via 2 options
    2 Factor Authentication Working for Emails.
    Mobile Authentication is still not working.

---
+++ task_management/models/__init__.py
@@ -0,0 +1,10 @@
+from .user_models import CustomUser
+from .contact_models import ContactQuery
+from .team_models import Team, TeamMember
+
+__all__ = [
+    'CustomUser',
+    'ContactQuery',
+    'Team',
+    'TeamMember',
+]
--- task_management/urls.py
+++ task_management/urls.py
@@ -1,24 +1,32 @@
-"""
-URL configuration for task_management project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.contrib import admin
 from django.urls import path, include
+from .views import auth_views, contact_views, dashboard_views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include('task_manager.urls')),
+
+    path('', auth_views.home_view, name='home'),
+    # Authentication URLs
+    path('api/signup/', auth_views.signup_view, name='signup'),
+    path('api/login/', auth_views.login_view, name='login'),
+    path('api/logout/', auth_views.logout_view, name='logout'), # Changed this line
+
+    # Contact paths
+    path('api/contact/', contact_views.submit_query, name='submit_query'),
+
+    # Dashboard URLs
+    path('dashboard/individual/student/', dashboard_views.individual_student_dashboard, name='individual_student_dashboard'),
+    
+    # Profile path
+    path('profile/', dashboard_views.profile_view, name='profile'),
+
+    # Profile image upload path and capture image
+    path('api/profile/upload-image/', dashboard_views.upload_profile_image, name='upload_profile_image'),
+    path('api/profile/capture-image/', dashboard_views.capture_profile_image, name='capture_profile_image'),
+
+    # Generate and Verify OTP
+    path('api/otp/', include([
+        path('generate/', dashboard_views.generate_verification_otp, name='generate_otp'),
+        path('verify/', dashboard_views.verify_otp, name='verify_otp'),
+        path('status/', dashboard_views.verification_status, name='verification_status'),  # New endpoint
+        path('resend/', dashboard_views.resend_otp, name='resend_otp'),  # New endpoint
+    ])),
 ]
